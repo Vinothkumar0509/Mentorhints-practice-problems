e438b170a9e46a87344002b70814665196bdfae9
Vinothkumar <vinothvino0518@gmail.com>
2025-11-04 11:21:16 +0530

    Added Fibonacci program

--- Practiseproblems/src/javaDsabasics/FibonacciSeries.java
+++ Practiseproblems/src/javaDsabasics/FibonacciSeries.java
@@ -1,7 +1,23 @@
 package javaDsabasics;
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int n = sc.nextInt();
+		int first =0;
+		int second =1;
+		
+		for(int i =2;i<n;i++)
+		{
+			int next = first+second;
+			System.out.println(" "+next);
+			first = second;
+			second = next;
+			
+		}
 		
 	}
 
